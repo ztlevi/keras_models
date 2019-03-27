75800af2c223c590bb4eb05a3f4802f11b3ea42b
Ting Zhou <zhouting@umich.edu>
2019-03-26 18:35:06 -0700

    dataset: add UTKface

---
+++ dataset/UTKFace.py
@@ -0,0 +1,91 @@
+import glob
+import numpy as np
+import os
+import pickle
+import random
+import sys
+
+import tables
+
+import cv2
+from definitions import UTKFace_DATASET_DIR
+
+
+def storage_add_images(img_storage, age_storage, gender_storage, race_storage, addrs, img_shape):
+    num_images = len(addrs)
+    invalid_addrs = []
+    for i, addr in enumerate(addrs):
+        if (i + 1) % 100 == 0 and i > 1:
+            sys.stdout.write(f"\rLog: {i + 1}/{num_images} images processed...")
+            sys.stdout.flush()
+
+        try:
+            img = cv2.imread(addr)
+        except Exception as e:
+            print(addr)
+            print(str(e))
+            continue
+
+        img = cv2.resize(img, img_shape, interpolation=cv2.INTER_CUBIC)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        img_storage.append(img[None])
+
+        # addr name formatted as [age]_[gender]_[race]_[data&time]
+        filename = os.path.basename(addr)
+        info = filename.split("_")
+        try:
+            age_storage.append([int(info[0])])
+            gender_storage.append([int(info[1])])
+            race_storage.append([int(info[2])])
+        except Exception as e:
+            invalid_addrs += [addr]
+            print(str(e))
+    print(f"\nFinish converting data!!!")
+    print(invalid_addrs)
+
+
+def dump_utkface_pkl(output_path):
+    storage_dtype = tables.UInt8Atom()
+
+    addrs = glob.glob(UTKFace_DATASET_DIR + "*.jpg")
+    random.shuffle(addrs)
+    print(len(addrs))
+
+    addrs_final, age_labels, gender_labels, race_labels = [], [], [], []
+    for addr in addrs:
+        filename = os.path.basename(addr)
+        info = filename.split("_")
+        try:
+            age = int(info[0])
+            gender = int(info[1])
+            race = int(info[2])
+        except Exception as e:
+            print(e)
+            continue
+        if age > 100:
+            age = 100
+        addrs_final += [addr]
+        age_labels += [age]
+        gender_labels += [gender]
+        race_labels += [race]
+
+    data = {
+        "addrs": np.array(addrs_final),
+        "gender_labels": np.array(gender_labels),
+        "age_labels": np.array(age_labels),
+        "race_labels": np.array(race_labels),
+    }
+    pickle.dump(data, open(output_path, "wb"))
+    print(f"\nFinish dumping data!!!")
+
+
+def get_utkface_dataset():
+    output_path = os.path.join(UTKFace_DATASET_DIR, "..", "UTKFace.pkl")
+    return pickle.load(open(output_path, "rb"))
+
+
+if __name__ == "__main__":
+    output_path = os.path.abspath(os.path.join(UTKFace_DATASET_DIR, "..", "UTKFace.pkl"))
+
+    dump_utkface_pkl(output_path)
---
+++ dataset/test/test_utkface.py
@@ -0,0 +1,60 @@
+from math import ceil
+from random import shuffle
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import norm
+from skimage.io import imread
+
+from dataset.UTKFace import get_utkface_dataset
+
+data = get_utkface_dataset()
+addrs = data["addrs"]
+gender_labels = data["gender_labels"]
+age_labels = data["age_labels"]
+
+num_images = len(addrs)
+
+# Plotting Age distribution
+sorted_age = sorted(age_labels)
+fit = norm.pdf(sorted_age, np.mean(sorted_age), np.std(sorted_age))
+plt.figure("Age distribution")
+plt.plot(sorted_age, fit, "-o")
+plt.hist(sorted_age, bins=20, density=True)
+plt.show()
+
+
+batch_size = 64
+num_class = 2
+
+# create list of batches to shuffle the data
+batches_list = list(range(int(ceil(float(num_images) / batch_size))))
+shuffle(batches_list)
+
+class_names = ["female", "male"]
+
+# loop over batches
+for x in range(5):
+    i = batches_list[x]
+
+    i_s = i * batch_size  # index of the first image in this batch
+    i_e = min([(i + 1) * batch_size, num_images])  # index of the last image in this batch
+
+    # read batch images and remove training mean
+    images = [imread(addr) for addr in addrs[i_s:i_e, ...]]
+
+    # read labels and convert to one hot encoding
+    seg_gender = gender_labels[i_s:i_e]
+    seg_age = age_labels[i_s:i_e]
+
+    plt.figure(figsize=(10, 10))
+    for j in range(25):
+        plt.subplot(5, 5, j + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        if j < len(images):
+            plt.imshow(images[j], cmap=plt.cm.binary)
+            plt.xlabel(class_names[seg_gender[j]] + "_" + str(seg_age[j]))
+
+plt.show()
