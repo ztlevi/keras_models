8123046c4cfb308cbd0d3dbf7e2cbf79dd1b90a8
Ting Zhou <zhouting@umich.edu>
2019-03-26 18:34:35 -0700

    update freeze script

--- utils/freeze_model.py
+++ utils/freeze_model.py
@@ -6,18 +6,30 @@ from tensorflow import keras
 from tensorflow.python.framework import graph_io
 
 from definitions import ROOT_DIR
+from training.age import mae_pred
 
 
 def freeze_graph(graph, session, output, output_path):
     with graph.as_default():
         graphdef_inf = tf.graph_util.remove_training_nodes(graph.as_graph_def())
-        graphdef_frozen = tf.graph_util.convert_variables_to_constants(session, graphdef_inf, output)
+        graphdef_frozen = tf.graph_util.convert_variables_to_constants(
+            session, graphdef_inf, output
+        )
         graph_io.write_graph(graphdef_frozen, output_path, "frozen_model.pb", as_text=False)
 
-keras.backend.set_learning_phase(0) # this line most important
 
-checkpoint_path = "/home/ztlevi/Developer/keras_models/outputs/checkpoints/gender_mobilenet_v1_imdb_wiki/ckpt-01-0.50.hdf5"
-model = keras.models.load_model(checkpoint_path)
+keras.backend.set_learning_phase(0)  # this line most important
+
+checkpoint_path = (
+    "/home/ztlevi/Developer/keras_models/outputs/checkpoints/age_mobilenet_v1_utkface/ckpt.h5"
+)
+model = keras.models.load_model(checkpoint_path, custom_objects={"mae_pred": mae_pred})
+
+# TUT age model
+# model = keras.models.load_model("/home/ztlevi/Developer/keras_models/outputs/tut/age/model.h5",
+#                                     custom_objects={'relu6': keras.layers.ReLU(6.),
+#                                                     'GlorotUniform': keras.initializers.glorot_uniform(),
+#                                                     'DepthwiseConv2D': keras.layers.DepthwiseConv2D})
 
 session = tf.keras.backend.get_session()
 
